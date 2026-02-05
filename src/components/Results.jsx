bddcc4bd6c1af24225adb8c13efc0d77f47e7e32
Luisa Velasquez <luvlasquez96@gmail.com>
2026-02-05 13:27:39 -0500

    feature: user input and results obj created

---
+++ src/components/Results.jsx
@@ -0,0 +1,8 @@
+export default function Results({ userInput }) {
+  return (
+    <section id="results">
+      <h2>Investment Parameters</h2>
+      {/* User input fields will go here */}
+    </section>
+  );
+}
